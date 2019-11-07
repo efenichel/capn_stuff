e62789e1561faf762479313967ad6f83609f4627
Eli Fenichel <eli.fenichel@yale.edu>
2019-11-07 11:51:39 -0500

    update main file and add data_setup
    I have made changes to my version of groundwater_capn.R.  This is the file I would prefer to work out of.  
    I packaged the system functions.  They pull from this branch (which I will fix later).
    I am reading the KSdata.RDS also from this branch (fix later).
    
    I have loaded the datasetup function, with a small tweak that specifies data set. 
    It is throwing an error 
    Error in mlogit.gmd.coeff[-c(1, 24, 25), ] : 
      incorrect number of dimensions
    
    Ideally, this will run and provide 
    csp.means, csp.raw, and gw.data.

---
+++ data_setup.R
@@ -0,0 +1,202 @@
+# -----------------------------------------------------------------------------
+# File Name: datasetup function
+# Purpose: Process data from regressions for the groundwater problem
+# Author:  Ethan Addicott
+# Date:  11/06/19
+# Notes: 
+
+#------------------------------------------------------------------------------
+
+# Convert regression output and summary statistics into capn parameters -------------------------------------
+datasetup <- function(gmdnum, dataset = ksdata) {
+  region_data <- ksdata
+  # Region Specific Parameters ---------------------------------------------------
+  #CASE 1: GMD 1-5
+  if (gmdnum < 6) {
+    # Crop Choice Parameters Setup ------------------------------------------------
+    `mlogit.gmd.coeff` <- region_data[[2]]
+    `mlogit.gmd.means` <- region_data[[3]]
+    
+    mlogit.gmd.coeff   <- data.frame(mlogit.gmd.coeff[-c(1,24,25),], 
+                                     stringsAsFactors = FALSE)
+    mlogit.gmd.means   <- data.frame(mlogit.gmd.means[-c(22,23), gmdnum + 1], 
+                                     stringsAsFactors = FALSE)
+    
+    # Transpose betas, chop off variable names to create crop.coeff column
+    crop.gmd.betas     <- t(mlogit.gmd.coeff[1,-c(1,ncol(mlogit.gmd.coeff))])
+    crop.gmd.alphas    <- rep(NA, ncol(mlogit.gmd.coeff) - 2)
+    crop.mean.vector <- append(as.numeric(unlist(mlogit.gmd.means[-c(1),])), 1)
+    max.j <- ncol(mlogit.gmd.coeff) - 1
+    for (j in 2:max.j) {
+      crop.gmd.alphas[j - 1] <- as.numeric(mlogit.gmd.coeff[-c(1), j]) %*% 
+        as.numeric(crop.mean.vector)
+    }
+    crop.coeff.gmd <- data.frame(crop.gmd.alphas,crop.gmd.betas, 
+                                 stringsAsFactors = FALSE)
+    colnames(crop.coeff.gmd) <- c("alpha","beta")
+    
+    # Crop Amounts Parameter Setup ------------------------------------------------
+    `crop.amts.gmd` <- region_data[[4]]
+    crop.amts.gmd <- data.matrix(crop.amts.gmd[-c(1,8),-c(1)])
+    crop.amts.gmd <- t(crop.amts.gmd)
+    crop.amts.gmd <- transform(crop.amts.gmd, numeric)
+    #Check that crop amounts are one more than 
+    if (nrow(crop.amts.gmd) != max.j) {
+      stop("BAD LENGTH")
+    }
+    # Water Withdrawal Parameter Setup --------------------------------------------
+    `water.coeff` <- region_data[[5]]
+    rmse <- as.numeric(water.coeff[36,2])
+    water.coeff <- data.frame(water.coeff[-c(1,34:37),] ,stringsAsFactors = FALSE)
+    
+    # Beta ------------------------------------------------------------------------
+    beta <- as.numeric(water.coeff[21,2])
+    
+    # Gamma -----------------------------------------------------------------------
+    gamma <- as.numeric(water.coeff[c(1:10),2])
+    gamma1 <- gamma[c(1,3,5,7,9)]
+    gamma2 <- gamma[c(2,4,6,8,10)]
+    
+    # Alpha -----------------------------------------------------------------------
+    alpha.water.coeff <- water.coeff[-c(1:11,21),2]
+    `wwd.means` <- region_data[[6]]
+    wwd.means <- data.frame(wwd.means[-c(1,11,22,23),gmdnum + 2], stringsAsFactors = FALSE)
+    wwd.mean.vector <- append(as.numeric(unlist(wwd.means)),1)
+    alpha <- as.numeric(alpha.water.coeff) %*% as.numeric(wwd.mean.vector)
+    alpha <- alpha + (rmse**2)/2 #correction for log estimation
+    alpha <- as.vector(alpha, mode = "numeric")
+  }
+  #CASE 2: GMD 6 Outgroup
+  else if (gmdnum == 6){
+    # Crop Choice Parameters Setup ------------------------------------------------
+    `mlogit.state.coeff` <- KSwater_data[[7]][[2]]
+    `mlogit.state.means` <- KSwater_data[[7]][[3]]
+    mlogit.state.coeff   <- data.frame(mlogit.state.coeff[-c(1,24,25),], 
+                                       stringsAsFactors = FALSE)
+    mlogit.state.means   <- data.frame(mlogit.state.means[-c(22,23),], 
+                                       stringsAsFactors = FALSE)
+    # Transpose betas, chop off variable names to create crop.coeff column
+    crop.state.betas     <- t(mlogit.state.coeff[1,-c(1,ncol(mlogit.state.coeff))])
+    crop.state.alphas    <- rep(NA, ncol(mlogit.state.coeff) - 2)
+    crop.mean.vector <- append(as.numeric(mlogit.state.means$mean[-c(1)]),1)
+    max.j <- ncol(mlogit.state.coeff) - 1
+    for (j in 2:max.j) {
+      crop.state.alphas[j - 1] <- as.numeric(mlogit.state.coeff[-c(1), j]) %*% 
+        as.numeric(crop.mean.vector)
+    }
+    crop.coeff.state <- data.frame(crop.state.alphas,crop.state.betas, 
+                                   stringsAsFactors = FALSE)
+    colnames(crop.coeff.state) <- c("alpha","beta")
+    
+    # Crop Amounts Parameter Setup ------------------------------------------------
+    `crop.amts.state` <- KSwater_data[[7]][[4]]
+    crop.amts.state <- data.matrix(crop.amts.state[-c(1,8),-c(1)])
+    crop.amts.state <- t(crop.amts.state)
+    
+    #Check that crop amounts are one more than 
+    if (nrow(crop.amts.state) != max.j) {
+      stop("BAD LENGTH")
+    }
+    
+    # Water Withdrawal Parameter Setup --------------------------------------------
+    `water.coeff` <- region_data[[5]]
+    rmse <- as.numeric(water.coeff[36,2])
+    water.coeff <- data.frame(water.coeff[-c(1,34:37),] ,stringsAsFactors = FALSE)
+    
+    # Beta ------------------------------------------------------------------------
+    beta <- as.numeric(water.coeff[21,2])
+    
+    # Gamma -----------------------------------------------------------------------
+    gamma <- as.numeric(water.coeff[c(1:10),2])
+    gamma1 <- gamma[c(1,3,5,7,9)]
+    gamma2 <- gamma[c(2,4,6,8,10)]
+    
+    # Alpha -----------------------------------------------------------------------
+    alpha.water.coeff <- water.coeff[-c(1:11,21),2]
+    `wwd.means` <- region_data[[6]]
+    wwd.means <- data.frame(wwd.means[-c(1,11,22,23),gmdnum + 2], stringsAsFactors = FALSE)
+    wwd.mean.vector <- append(as.numeric(unlist(wwd.means)),1)
+    alpha <- as.numeric(alpha.water.coeff) %*% as.numeric(wwd.mean.vector)
+    alpha <- alpha + (rmse**2)/2 #correction for log estimation
+    alpha <- as.vector(alpha, mode = "numeric")
+  }
+  #CASE 3: STATE
+  else if (gmdnum == 7){
+    # Crop Choice Parameters Setup ------------------------------------------------
+    `mlogit.state.coeff` <- region_data[[2]]
+    `mlogit.state.means` <- region_data[[3]]
+    mlogit.state.coeff   <- data.frame(mlogit.state.coeff[-c(1,24,25),], 
+                                       stringsAsFactors = FALSE)
+    mlogit.state.means   <- data.frame(mlogit.state.means[-c(22,23),], 
+                                       stringsAsFactors = FALSE)
+    # Transpose betas, chop off variable names to create crop.coeff column
+    crop.state.betas     <- t(mlogit.state.coeff[1,-c(1,ncol(mlogit.state.coeff))])
+    crop.state.alphas    <- rep(NA, ncol(mlogit.state.coeff) - 2)
+    crop.mean.vector <- append(as.numeric(mlogit.state.means$mean[-c(1)]),1)
+    max.j <- ncol(mlogit.state.coeff) - 1
+    for (j in 2:max.j) {
+      crop.state.alphas[j - 1] <- as.numeric(mlogit.state.coeff[-c(1), j]) %*% 
+        as.numeric(crop.mean.vector)
+    }
+    crop.coeff.state <- data.frame(crop.state.alphas,crop.state.betas, 
+                                   stringsAsFactors = FALSE)
+    colnames(crop.coeff.state) <- c("alpha","beta")
+    
+    # Crop Amounts Parameter Setup ------------------------------------------------
+    `crop.amts.state` <- region_data[[4]]
+    crop.amts.state <- data.matrix(crop.amts.state[-c(1,8),-c(1)])
+    crop.amts.state <- t(crop.amts.state)
+    
+    #Check that crop amounts are one more than 
+    if (nrow(crop.amts.state) != max.j) {
+      stop("BAD LENGTH")
+    }
+    
+    # Water Withdrawal Parameter Setup --------------------------------------------
+    `water.coeff` <- region_data[[5]]
+    rmse <- as.numeric(water.coeff[36,2])
+    water.coeff <- data.frame(water.coeff[-c(1,34:37),] ,stringsAsFactors = FALSE)
+    
+    # Beta ------------------------------------------------------------------------
+    beta <- as.numeric(water.coeff[21,2])
+    
+    # Gamma -----------------------------------------------------------------------
+    gamma <- as.numeric(water.coeff[c(1:10),2])
+    gamma1 <- gamma[c(1,3,5,7,9)]
+    gamma2 <- gamma[c(2,4,6,8,10)]
+    
+    # Alpha -----------------------------------------------------------------------
+    alpha.water.coeff <- water.coeff[-c(1:11,21),2]
+    `wwd.means` <- region_data[[6]]
+    wwd.means <- data.frame(wwd.means[-c(1,11,22,23),], stringsAsFactors = FALSE)
+    wwd.mean.vector <- append(as.numeric(wwd.means$all),1)
+    alpha <- as.numeric(alpha.water.coeff) %*% as.numeric(wwd.mean.vector)
+    alpha <- alpha + (rmse**2)/2 #correction for log estimation
+    alpha <- as.vector(alpha, mode = "numeric")
+  }
+  
+  # CONSISTENT PARAMETERS ------------------------------------------------------
+  # Cost Crop Acre 
+  `cost.crop.acre` <- region_data[[7]]
+  cost.crop.acre <- as.vector(unlist(cost.crop.acre), mode = "numeric")
+  
+  # Crop Prices 
+  `crop.prices` <- region_data[[8]]
+  crop.prices <- as.vector(unlist(crop.prices), mode = "numeric")
+  
+  
+  # Store Parameters in Data Structure ------------------------------------------
+  gw.data <- rep(NA,9) #preallocate
+  # GMD 1-5
+  if (gmdnum < 6) {
+    gw.data <- list(crop.coeff.gmd, crop.amts.gmd, alpha, beta, gamma, 
+                    gamma1, gamma2, crop.prices, cost.crop.acre)
+  }
+  # GMD 6 (Outgroup) or State
+  if (gmdnum >= 6) {
+    gw.data <- list(crop.coeff.state, crop.amts.state, alpha, beta, gamma, 
+                    gamma1, gamma2, crop.prices, cost.crop.acre)
+  }
+  
+  return(gw.data)
+} 
--- groundwater_capn.R
+++ groundwater_capn.R
@@ -8,7 +8,7 @@
 if (!require("pacman")) install.packages("pacman") #pacman allows you to use the p_load function
 #the p_load function checks is a library is installed, if not it installs it, then it attaches the 
 #called library
-p_load(capn, R.oo, repmis, ggplot2)
+p_load(capn, R.oo, repmis, ggplot2, RCurl)
 
 #capn documentation: https://cran.r-project.org/web/packages/capn/capn.pdf 
 #repmis documentation: https://cran.r-project.org/web/packages/repmis/index.html 
@@ -17,7 +17,12 @@ rm(list=ls()) #clear workspace
 
 #get data set for the problem set from Github
 source_data("https://github.com/efenichel/capn_stuff/raw/master/my_gw_data.RData")
-                     
+
+
+ksdata <-readRDS(gzcon(url("https://github.com/efenichel/capn_stuff/raw/forspi/KSwater_data.RDS")))
+
+datasetup(2)
+
 #The elements of gw.data are
 #The parameters from the multinomial logit for crop shares, 
 #to know which is which see the labels on the additional csp.means data using the View(csp.means) command.
@@ -44,150 +49,17 @@ recharge <- 1.25 #inches per year constant rate
 order <- 10 # approximaton order
 NumNodes <- 100 #number of nodes
 
-
-
-####functions from Fenichel et al. 2016
 #####################################################################
-#Area planting decision model
-cropFwater<- function(water, param){
-  
-  cropSharePar<-param[[1]]
-  cropAmountPar<-param[[2]]
-  
-  contr <- exp(cropSharePar$crop.share.a+water*cropSharePar$crop.share.b)
-  h <- sum(contr)
-  contr <- c(contr, 1)
-  
-  cropout <- matrix(0,nrow=length(contr),ncol=6)
-  
-  for(j in 1:6){
-    cropout[,j]<-contr/(1+h)*cropAmountPar[,j]
-  }
-  
-  rowSums(t(cropout))
-  
-}
+# Get the system model associated with Fenichel et al. 2016
+# First load the functions 
+#source('system_fns.R')  
 
+source("https://raw.githubusercontent.com/efenichel/capn_stuff/forspi/system_fns.R") #will need to fix in the end
 
-#####################################################################
-#Derivative of area planting decision model
-DcropFwater<- function(water, param){
-  
-  cropSharePar<-param[1][[1]]
-  cropAmountPar<-param[2][[1]]
-  
-  contr <- exp(cropSharePar$crop.share.a+water*cropSharePar$crop.share.b)
-  h <- sum(contr)
-  hp <- sum(contr*cropSharePar$crop.share.b)
-  
-  contr <- c(contr, 1)
-  bv <- c(cropSharePar$crop.share.b,0)
-  
-  Dcropout <- matrix(0,nrow=length(contr),ncol=6)
-  
-  for(j in 1:6){
-    Dcropout[,j]<- cropAmountPar[,j]*contr*(bv+bv*h-hp)/((1+h)^2)
-  }
-  
-  rowSums(t(Dcropout))
-  
-}
+#notice in the explore tab there are now a bunch of Functions.
+#you can click on any function and see the functions that support the system model.
+#These are the functions that you need to build and calibrate for your system.
 
-#####################################################################
-#Water withdrawl function
-
-Wwd<-function(water,param){
-  
-  #alpha<-as.data.frame(lapply(param[[4]],identity))
-  alpha<-param[[3]]
-  #beta<-as.data.frame(lapply(param[[5]],identity))
-  beta<-param[[4]]
-  Gamma<-param[[5]]
-  
-  AP1<-cropFwater(water,param) #area planted
-  AP <- matrix(0,nrow=1,ncol=6) #normalized area planted
-  for(j in 1:6){
-    AP[j]<-AP1[j]/sum(AP1)
-  }
-  
-  #area planted and arrea planted squared
-  z<-matrix(0,nrow=1,ncol=10)
-  z[1]=AP1[1]
-  z[2]=(AP1[1])^2
-  z[3]=AP1[2]
-  z[4]=(AP1[2])^2
-  z[5]=AP1[3]
-  z[6]=(AP1[3])^2
-  z[7]=AP1[4]
-  z[8]=(AP1[4])^2
-  z[9]=AP1[5]
-  z[10]=(AP1[5])^2
-  
-  wx<-exp(alpha+water*beta+sum(z*Gamma))/sum(AP1) #projects based on regession model
-  min(wx,water) #imposes no shorting water
-  
-}
-#####################################################################
-#Derivative of water withdrawl function
-WwdDs1<-function(water,param){
-  Wwd <- Wwd(water,param)
-  
-  beta<-param[[4]][[1]]
-  Gamma1<-param[[6]]
-  Gamma2<-param[[7]]
-  
-  AP1<-cropFwater(water,param) #area planted
-  APprime1 <- DcropFwater(water,param) #derivative of the area planted function
-  
-  temp<-sum(APprime1[1:5]*Gamma1)+sum((2*APprime1[1:5]*AP1[1:5])*Gamma2)
-  Wwd*(beta+temp)
-  
-}
-#####################################################################
-#profit function
-profit <- function(water,param){
-  
-  prices<-t(param[[8]])
-  costCropAcreX<-param[[9]]
-  
-  AP1<-cropFwater(water,param) #area planted
-  AP <- matrix(0,nrow=1,ncol=6) #normalized area planted
-  for(j in 1:6){
-    AP[j]<-AP1[j]/sum(AP1)
-  }
-  temp<-c(as.numeric(prices[1:5,1]-costCropAcreX),0)
-  sum(AP[1:6]*(temp[1:6]))
-  
-  
-}
-
-#####################################################################
-#profit prime s function
-ProfDs1 <- function(water,param){
-  
-  prices<-t(param[[8]])
-  costCropAcreX<-param[[9]]
-  
-  AP1<-cropFwater(water,param) #area planted
-  APprime1 <- DcropFwater(water,param) #derivative of the area planted function
-  
-  
-  APprime1pa <- matrix(0,nrow=1,ncol=6) #normalized area planted
-  for(j in 1:6){
-    APprime1pa[j]<-APprime1[j]/sum(AP1)
-  }
-  
-  temp<-c(as.numeric(prices[1:5,1]-costCropAcreX),0)
-  sum(APprime1pa[1:6]*(temp[1:6]))
-  
-  
-}
-
-#####################################################################
-#change in groundwater with respect to time
-sdot <- function(water,recharge,param){
-  recharge/12 - Wwd(water,param)
-}
 ######################################################################
 ######################################################################
 ######################################################################
